905ec93e92ac56e86ea785cb056e0abba5a7e170
Sigal Shaharabani <sigals@tikalk.com>
2019-07-16 13:59:21 +0300

    Experiment service

--- src/main/java/com/tikalk/chaostoolkitrunner/controller/ExperimentController.java
+++ src/main/java/com/tikalk/chaostoolkitrunner/controller/ExperimentController.java
@@ -1,7 +1,29 @@
 package com.tikalk.chaostoolkitrunner.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.tikalk.chaostoolkitrunner.service.ExperimentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "gr7/experiment", produces = "application/json")
 public class ExperimentController {
+
+    private ExperimentService experimentService;
+
+    @Autowired
+    public ExperimentController(ExperimentService experimentService) {
+        this.experimentService = experimentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Integer> runExperiment() {
+        return new ResponseEntity<>(experimentService.launch(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getResult(@PathVariable int id) {
+        return new ResponseEntity<>(experimentService.getJournal(id), HttpStatus.OK);
+    }
 }
---
+++ src/main/java/com/tikalk/chaostoolkitrunner/service/ExperimentService.java
@@ -0,0 +1,27 @@
+package com.tikalk.chaostoolkitrunner.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Chaos engineering experiments service
+ */
+@Service
+public class ExperimentService {
+
+    /**
+     * Launch the defined experiment
+     * @return Run-id
+     */
+    public Integer launch() {
+        return null;
+    }
+
+    /**
+     * Get the journal with the results of an experiment run
+     * @param id Run-id
+     * @return Journal JSon
+     */
+    public String getJournal(int id) {
+        return null;
+    }
+}

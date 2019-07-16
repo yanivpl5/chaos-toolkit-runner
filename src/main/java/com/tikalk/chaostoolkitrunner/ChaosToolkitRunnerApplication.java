d947a52e27e5db437b5bc454012cb07058892f2d
Yaniv Peleg <yanivpl5@gmail.com>
2019-07-16 13:38:37 +0300

    initial commit

---
+++ src/main/java/com/tikalk/chaostoolkitrunner/ChaosToolkitRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.tikalk.chaostoolkitrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaosToolkitRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaosToolkitRunnerApplication.class, args);
+	}
+
+}
